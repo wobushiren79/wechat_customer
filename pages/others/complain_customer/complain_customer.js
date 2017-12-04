eaccba438e169270faee14eec4dc35fcf5452151
xGangg <13547965719@163.com>
2017-12-04 09:03:47 +0800

    客户-投诉

--- pages/others/complain_customer/complain_customer.js
+++ pages/others/complain_customer/complain_customer.js
@@ -1,26 +1,32 @@
 // pages/others/complain_customer.js
-var ModalUtil = require('../../../utils/ModalUtil.js');
-var content;
+var modalUtil = require('../../../utils/ModalUtil.js');
+var toastUtil= require('../../../utils/ToastUtil.js');
+var platformUrl=require('../../../utils/http/RequestForPlatform.js');
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-	complaintType: 0,
 	complaintType_SelectedIndex:0,
 	complaintType_Set:[
 		{ id: 0, text: '--未选择--'},
 		{ id: 1, text: '公墓' },
 		{ id: 2, text: '殡仪'},
 		{ id: 3, text: '人员'}
-	]
+	],
+	complaintType: null,
+	pagesPositionUrl: null,
+	complaintPicture:null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-	  content=this;
+	  var pagesPositionUrlObj = getApp().pagesPositionUrl;
+	  this.setData({
+		  pagesPositionUrl: pagesPositionUrlObj
+	  });
   },
 
   /**
@@ -74,7 +80,7 @@ Page({
   /**
    * 类型选择
    */
-  complaintTypeChange: function(e){
+  complaintTypeChange: function(e){	  
 	  var selectIndex = parseInt(e.detail.value);
 	  var selectItem = this.data.complaintType_Set[selectIndex];
 	  this.setData({
@@ -82,9 +88,63 @@ Page({
 		  complaintType: selectItem.id
 	  });
   },
-  cancelComplain:function(e){
-	  ModalUtil.showModal('提示','需要取消？', function(){
-		  var C_aboutme = getApp().pagesPositionUrl['C_aboutme'];
+  wireValideFormObj: function(name, msg){
+	var obj={};
+	obj.name=name;
+	obj.msg=msg;
+	return obj;
+  },
+  requireFormDataWire: function(){
+	  var requireFormData = [];
+	  requireFormData.push(this.wireValideFormObj('complaintType','类型，需要选择'));
+	  requireFormData.push(this.wireValideFormObj('complaintObjcet', '名称，未填入'));
+	  requireFormData.push(this.wireValideFormObj('complaintContent', '内容，未填入'));
+	  return requireFormData;
+  },
+  valideFormData: function (formDataObj, requireFormData) {
+	  if (formDataObj == null || requireFormData == null ||
+		  requireFormData.length == null || requireFormData.length == 0) {
+		  return false;
+	  }
+	  var isFill=true;
+	  var msg=null;
+	  for (var i in requireFormData) {
+		  var item=requireFormData[i];
+		  var itemVal = formDataObj[item.name];
+		  if (itemVal == null || (typeof (itemVal)=='string'&& itemVal.length == 0)){
+			  isFill = false;
+			  msg = item.msg;
+			  break;
+		  }
+	  }
+	  if (!isFill){
+		  toastUtil.showToast(msg);
+		  return false;
+	  }
+	  return true;
+  },
+  formData: function (e) {
+	  var formDataObj = e.detail.value;
+	  formDataObj.complaintType = this.data.complaintType;
+	  formDataObj.complaintPicture = this.data.complaintPicture;
+	  var requireFormData = this.requireFormDataWire();
+	  var isRealy=this.valideFormData(formDataObj, requireFormData);
+	  if (!isRealy){
+		  return;
+	  }
+	  var resp={
+		  success:function(data,res){
+			  console.log(res);
+		  },
+		  fail:function(data,res){
+			  console.log(res);
+		  }
+	  };	
+	  platformUrl.submitComplaints(formDataObj, resp);
+  },
+  cancelComplain: function(e){
+	  modalUtil.showModal('提示','需要取消？', function(){
+		  var C_aboutme = pagesPositionUrl['C_aboutme'];
 		  wx.redirectTo({
 			  url: C_aboutme
 		  });
