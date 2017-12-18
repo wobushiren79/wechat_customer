d07d767c885eba6c50b9d6095b2e0ee58fb06044
LiuFL <Amelia_liu@qq.com>
2017-12-18 11:38:32 +0800

    公墓列表平台不登录

--- utils/util.js
+++ utils/util.js
@@ -9,15 +9,16 @@ function formatTime(date) {
   var second = date.getSeconds()
 
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  return [year, month, day].map(formatNumber).join('-') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
-function formatDate(date) {
-  var year = date.getFullYear()
-  var month = date.getMonth() + 1
-  var day = date.getDate()
-
-  return [year, month, day].map(formatNumber).join('/') 
+function formatDate(mescStr) {
+  var n = mescStr;
+  var date = new Date(n);
+  var Y = date.getFullYear() + '-';
+  var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+  var D = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+  return (Y + M + D) 
 }
 
 function formatNumber(n) {
@@ -26,7 +27,41 @@ function formatNumber(n) {
 
 }
 
+/** 
+ * 时间戳转化为年 月 日 时 分 秒 
+ * number: 传入时间戳 
+ * format：返回格式，支持自定义，但参数必须与formateArr里保持一致 
+*/
+function formatDateTime(number, format) {
+
+  var formateArr = ['Y', 'M', 'D', 'h', 'm', 's'];
+  var returnArr = [];
+
+  var date = new Date(number * 1000);
+  returnArr.push(date.getFullYear());
+  returnArr.push(formatNumber(date.getMonth() + 1));
+  returnArr.push(formatNumber(date.getDate()));
+
+  returnArr.push(formatNumber(date.getHours()));
+  returnArr.push(formatNumber(date.getMinutes()));
+  returnArr.push(formatNumber(date.getSeconds()));
+
+  for (var i in returnArr) {
+    format = format.replace(formateArr[i], returnArr[i]);
+  }
+  return format;
+} 
+
+function generateOrderNumber(cemeteryName){
+  var Str = cemeteryName;
+  var timestamp = Date.parse(new Date());
+  timestamp = timestamp / 1000;
+  return cemeteryName+'GLF'+timestamp;
+}
+
 module.exports = {
   formatTime: formatTime,
-  formatDate: formatDate
+  formatDate: formatDate,
+  generateOrderNumber: generateOrderNumber,
+  formatDateTime: formatDateTime
 }
