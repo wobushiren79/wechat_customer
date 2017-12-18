0b371c35725b56dda81503a0431e8cde95c334ff
LiuFL <Amelia_liu@qq.com>
2017-12-18 19:12:24 +0800

    no

--- utils/http/RequestForCemetery.js
+++ utils/http/RequestForCemetery.js
@@ -11,16 +11,40 @@ function loginCemetery(data, callback) {
  * 查询墓位信息
  */
 function findPositionByCondition(data, callback) {
-  baseRequest.sendPostHttpForContent(baseUrl + "marketing/fee/findPositionByCemeteryIdAndCondition", data, callback, true)
+    baseRequest.sendPostHttpForContent(baseUrl + "marketing/fee/findPositionByCemeteryIdAndCondition", data, callback, true)
 }
 
 /**
  * 查询墓位详情
  */
 function queryMgtFeeByPositionId(data, callback) {
-  baseRequest.sendPostHttpForContent(baseUrl + "marketing/fee/queryMgtFeeByPositionId", data, callback, true)
+    baseRequest.sendPostHttpForContent(baseUrl + "marketing/fee/queryDetailByPositionId", data, callback, true)
 }
 
-module.exports.findPositionByCondition = findPositionByCondition;      
-module.exports.queryMgtFeeByPositionId = queryMgtFeeByPositionId;  
-module.exports.loginCemetery = loginCemetery;   
+/**
+ * 查询墓位缴费记录
+ */
+function queryMgtFeePage(data, callback) {
+    baseRequest.sendPostHttpForContent(baseUrl + "marketing/fee/queryMgtFeePage", data, callback, true)
+}
+
+/**
+ * 缴管理费
+ */
+function payMgt(data, callback) {
+    baseRequest.sendPostHttpForContent(baseUrl + "marketing/fee/payMgt", data, callback, true)
+}
+
+/**
+ * 更新管理费商户订单号
+ */
+function updateOutTradeNo(data, callback) {
+  baseRequest.sendPostHttpForContent(baseUrl + "marketing/fee/updateOutTradeNo", data, callback, true)
+}
+
+module.exports.findPositionByCondition = findPositionByCondition;
+module.exports.queryMgtFeeByPositionId = queryMgtFeeByPositionId;
+module.exports.loginCemetery = loginCemetery;
+module.exports.queryMgtFeePage = queryMgtFeePage;
+module.exports.payMgt = payMgt;
+module.exports.updateOutTradeNo = updateOutTradeNo;
